e74ab2df3a206abe88c5ff88b60a07f1aac2a5a6
tnguyen28 <tnguyen28@luc.edu>
2017-10-16 21:40:09 -0500

    push

--- src/main/java/edu/luc/cs271/linkedstack/LinkedStack.java
+++ src/main/java/edu/luc/cs271/linkedstack/LinkedStack.java
@@ -2,43 +2,64 @@ package edu.luc.cs271.linkedstack;
 
 import java.util.List;
 import java.util.ArrayList;
+import java.util.NoSuchElementException;
 
 
 public class LinkedStack<E> implements IStack<E> {
 
   /** The topmost node of this stack. The stack gets pushed down from here. */
   private Node<E> top;
-
   // TODO why don't we need an explicit constructor?
 
   @Override
   public E push(final E obj) {
     // TODO
-    return null;
+    top = new Node<E>(obj,top);
+    return obj;
   }
 
   @Override
   public E peek() {
     // TODO
-    return null;
+    if(top == null){
+      throw new NoSuchElementException();
+    } else{
+      return top.data;
+    }
   }
 
   @Override
   public E pop() {
     // TODO
-    return null;
+    if(top == null){
+      throw new NoSuchElementException();
+    } else {
+      E temp = top.data;
+      top = top.next;
+      return temp;
+    }
   }
 
   @Override
   public boolean isEmpty() {
     // TODO
-    return false;
+    if(top == null){
+      return true;
+    } else {
+      return false;
+    }
   }
 
   @Override
   public List<E> asList() {
     // TODO implement using an ArrayList preallocated with the right size
     // TODO add any instance variable(s) required to support this
-    return null;
+    List<E> list = new ArrayList<E>();
+
+    while(top != null){
+      list.add(top.data);
+      top = top.next;
+    }
+    return list;
   }
 }
--- src/main/java/edu/luc/cs271/linkedstack/ReverseLines.java
+++ src/main/java/edu/luc/cs271/linkedstack/ReverseLines.java
@@ -3,20 +3,28 @@ package edu.luc.cs271.linkedstack;
 import java.util.Scanner;
 
 public class ReverseLines {
-
 	public static void main(String[] args) {
 		// TODO read successive input lines until EOF, then print out in reverse order
 
+		LinkedStack<Character> s = new LinkedStack<Character>();
 
-
-    final Scanner input = new Scanner(System.in);
+		System.out.println("Please hit the Enter key to start the program...");
+		final Scanner input = new Scanner(System.in);
 		String line;
 		while ((line = input.nextLine()) != null) {
+			line = input.next();
+			System.out.println(line);
+			for(int i = 0; i < line.length(); i++){
+				s.push(line.charAt(i));
+			}
 
+			String print = "";
+			for(int i = 0; i < line.length(); i++){
+				print += s.pop();
+			}
 
-
+			System.out.println(print);
 		}
-
 
 
 	}
